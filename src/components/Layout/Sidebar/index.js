f0dfca22baf39127e7191d5be417c19ee413ef8b
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-27 13:26:02 +0530

    Added Social Media Icons

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -6,6 +6,7 @@ import School from "@mui/icons-material/SchoolOutlined";
 import Interests from "@mui/icons-material/InterestsOutlined";
 import Experience from "@mui/icons-material/WorkHistoryOutlined";
 import Contact from "@mui/icons-material/ContactsOutlined";
+import { Icon } from "@iconify/react";
 
 function SideBar() {
   return (
@@ -16,42 +17,72 @@ function SideBar() {
       >
         <img src={logo} alt="logo" className="w-56"></img>
         <nav className="flex flex-col justify-center">
-          <NavLink exact="true" activeclassname="active" to="/" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/"
+            className="sidebar-option"
+          >
             <HomeIcon
               className="sidebar-icon"
-              style={{ fontSize: "1.9rem"}}
+              style={{ fontSize: "1.9rem" }}
             ></HomeIcon>
             <h1 className="sidebar-text">Home</h1>
           </NavLink>
-          <NavLink exact="true" activeclassname="active" to="/experience" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/experience"
+            className="sidebar-option"
+          >
             <Experience
               className="sidebar-icon"
               style={{ fontSize: "1.9rem" }}
             ></Experience>
             <h1 className="sidebar-text">Experience</h1>
           </NavLink>
-          <NavLink exact="true" activeclassname="active" to="/skills" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/skills"
+            className="sidebar-option"
+          >
             <WorkIcon
               className="sidebar-icon"
               style={{ fontSize: "1.9rem" }}
             ></WorkIcon>
             <h1 className="sidebar-text">Skills</h1>
           </NavLink>
-          <NavLink exact="true" activeclassname="active" to="/education" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/education"
+            className="sidebar-option"
+          >
             <School
               className="sidebar-icon"
               style={{ fontSize: "1.9rem" }}
             ></School>
             <h1 className="sidebar-text">Education</h1>
           </NavLink>
-          <NavLink exact="true" activeclassname="active" to="/interests" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/interests"
+            className="sidebar-option"
+          >
             <Interests
               className="sidebar-icon"
               style={{ fontSize: "1.9rem" }}
             ></Interests>
             <h1 className="sidebar-text">Interests</h1>
           </NavLink>
-          <NavLink exact="true" activeclassname="active" to="/contact_me" className="sidebar-option">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/contact_me"
+            className="sidebar-option"
+          >
             <Contact
               className="sidebar-icon"
               style={{ fontSize: "1.9rem" }}
@@ -59,6 +90,24 @@ function SideBar() {
             <h1 className="sidebar-text">Contact Me</h1>
           </NavLink>
         </nav>
+        <div className="flex flex-row h-screen items-end ">
+          <Icon
+            icon="mdi:linkedin"
+            className="sidebar-profileIcon"
+          />
+          <Icon
+            icon="carbon:logo-github"
+            className="sidebar-profileIcon"
+          />
+          <Icon
+            icon="cib:leetcode"
+            className="sidebar-profileIcon"
+          />
+          <Icon
+            icon="simple-icons:geeksforgeeks"
+            className="sidebar-profileIcon"
+          />
+        </div>
       </Link>
     </div>
   );
