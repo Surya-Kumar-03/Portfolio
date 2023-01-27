aaf7f1bc8ecad315d6ee9f92b577fecf881b18ce
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-27 23:21:55 +0530

    Home Page Edited!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,23 @@
+import Logo from "../../assets/logo.png";
+
 function Home() {
-  return <div>Hello</div>;
+  return (
+    // Container
+    <div>
+      {/* Text */}
+      <div>
+        <h1>
+          Hi, <br></br> I'm
+          <img src={Logo} alt="Software Engineer"></img>
+          Bandepalli Surya
+          <br></br>I utilise technology to address problems.
+        </h1>
+        <h2>
+          
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
