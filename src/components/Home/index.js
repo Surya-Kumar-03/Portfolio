310fe729628834e2f64cff556efbd523ee5ed0e0
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-29 10:34:59 +0530

    Animation for Fonts.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,17 +5,14 @@ function Home() {
     // Container
     <div className="home-container">
       {/* Text */}
-      <p className="text-4xl m-2 text-styling">Hi, I'm</p>
+      <p className="text-4xl m-2 text-styling animateFont-before">Hi, I'm</p>
+
       <h1 className="flex flex-row items-end mt-2 mb-2">
-        <img
-          src={SLetter}
-          alt="Software Engineer"
-          className="schar-style"
-        ></img>
+        <img src={SLetter} alt="Surya" className="schar-style"></img>
         <h1 className="-ml-3 text-5xl">urya</h1>
       </h1>
       <div className="flex flex-col items-center">
-        <h1 className="text-styling page-alignStyles">
+        <h1 className="text-styling text-4xl m-4 text-center animateFont-after">
           I utilise technology to solve problems.
         </h1>
         {/* <h2></h2> */}
