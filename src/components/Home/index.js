01643f46abdea0cda64613e8ab62b7e1f062e955
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-29 15:45:19 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,9 @@ import Logo from "../LogoHome";
 function Home() {
   return (
     // Container
-    <div className="flex flex-col sm:flex-row sm:w-screen h-[85vh] sm:h-screen 
-    justify-center sm:items-center">
+    <div
+      className="flex flex-col sm:flex-row sm:w-screen h-[85vh] sm:h-screen justify-center sm:items-center"
+    >
       <div className="home-container">
         {/* Text */}
         <p className="text-4xl m-2 text-styling animateFont-before">Hi, I'm</p>
@@ -16,7 +17,10 @@ function Home() {
         </h1>
         <div className="flex flex-col items-center">
           <h1 className="text-styling text-4xl m-4 text-center animateFont-after">
-            I utilise technology to solve problems.
+            I utilize technology to solve problems.
+          </h1>
+          <h1 className="text-xl font-thin text-center animateFont-after">
+            Hover around to discover more about me!{" "}
           </h1>
           {/* <h2></h2> */}
         </div>
