8e2a84cec5a0420d02a1d7d8aae5e84c9467aa34
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-01-29 11:10:47 +0530

    Name Animation Added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ function Home() {
 
       <h1 className="flex flex-row items-end mt-2 mb-2">
         <img src={SLetter} alt="Surya" className="schar-style"></img>
-        <h1 className="-ml-3 text-5xl">urya</h1>
+        <h1 className="-ml-3 text-5xl animateName">urya</h1>
       </h1>
       <div className="flex flex-col items-center">
         <h1 className="text-styling text-4xl m-4 text-center animateFont-after">
